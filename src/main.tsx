31f69bcac8b6049ea41f4e17f469fde40b670555
Md Rashedul Islam <rajib5570@gmail.com>
2024-10-03 18:09:48 +0600

    navbar added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RouterProvider router={router}>
+
     <App />
+    </RouterProvider>
   </StrictMode>,
 )
