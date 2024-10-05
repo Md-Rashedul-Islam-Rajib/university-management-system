4c77f761f1eebc5f2e8134a0c5b57f5f6b98264f
Md Rashedul Islam <rajib5570@gmail.com>
2024-10-05 19:16:07 +0600

    new 40 course data added

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,18 +1,29 @@
+import StudentPortalBanner from "@/components/shared/StudentPortalBanner";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-import StudentPortalBanner from '@/components/shared/StudentPortalBanner'
 
 const Courses = () => {
   return (
     <div>
-        <div className="m-6">
+      <div className="m-6">
         <StudentPortalBanner />
-        </div>
-        <div>
-            
-        </div>
-      
+      </div>
+      <div>
+        <Tabs defaultValue="current-courses" className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="current-courses">Current Courses</TabsTrigger>
+            <TabsTrigger value="finished-courses">Finished Courses</TabsTrigger>
+            <TabsTrigger value="dropped-courses">Dropped Courses</TabsTrigger>
+          </TabsList>
+          <TabsContent value="current-courses">
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value="finished-courses">Change your password here.</TabsContent>
+          <TabsContent value="dropped-courses">Change your password here.</TabsContent>
+        </Tabs>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Courses
+export default Courses;
